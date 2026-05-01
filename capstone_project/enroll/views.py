5ae55d647cfe925cad8931ae62e0ff72cb255893
mgurjar9098-sudo <m.gurjar9098@gmail.com>
2026-05-01 19:34:11 +0530

    code

--- capstone_project/enroll/views.py
+++ capstone_project/enroll/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 
+from .models import User
+from .forms import StudentsRegisteration
+
 # Create your views here.
 
 def add_show(request):
-    return render(request,'enroll/add_show.html')
+    if request.method == 'POST':
+        fm=StudentsRegisteration(request.POST)
+        if fm.is_valid():
+            nm=fm.cleaned_data['name']
+            em=fm.cleaned_data['email']
+            pw=fm.cleaned_data['password']
+            reg =User(name=nm,email=em,password=pw)
+            reg.save()
+            fm=StudentsRegisteration()
+            # fm.save()
+    else: 
+        fm=StudentsRegisteration()
+    stud=User.objects.all()
+    print(stud)
+    return render(request,'enroll/add_show.html',{'form':fm})
